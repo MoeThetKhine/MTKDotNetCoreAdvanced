6a952867e6858675c833fca6ba4b62fe8a89aaa4
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-03 12:31:06 +0630

    -

--- MTKDotNetCoreAdvancedC#.Utils/Resources/MessageResource.cs
+++ MTKDotNetCoreAdvancedC#.Utils/Resources/MessageResource.cs
@@ -1,7 +1,11 @@
 ﻿namespace MTKDotNetCoreAdvancedC_.Utils.Resources;
 
+#region MessageResource
+
 public class MessageResource
 {
     public static string Success { get; } = "Success";
     public static string Fail { get; } = "Fail.";
 }
+
+#endregion
