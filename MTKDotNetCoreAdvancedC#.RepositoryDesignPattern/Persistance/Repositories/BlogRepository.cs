ae5dd7ae68f5fa69ac4f4594ab779ba8aa22209d
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-03 12:39:48 +0630

    Format Code

--- MTKDotNetCoreAdvancedC#.RepositoryDesignPattern/Persistance/Repositories/BlogRepository.cs
+++ MTKDotNetCoreAdvancedC#.RepositoryDesignPattern/Persistance/Repositories/BlogRepository.cs
@@ -1,54 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
-using MTKDotNetCoreAdvancedC_.Database.Models;
-using MTKDotNetCoreAdvancedC_.RepositoryDesignPattern.Models;
-using MTKDotNetCoreAdvancedC_.Utils;
+﻿namespace MTKDotNetCoreAdvancedC_.RepositoryDesignPattern.Persistance.Repositories;
 
-namespace MTKDotNetCoreAdvancedC_.RepositoryDesignPattern.Persistance.Repositories
+public class BlogRepository : IBlogRepository
 {
-    public class BlogRepository : IBlogRepository
+    internal readonly AppDbContext _context;
+
+    public BlogRepository(AppDbContext context)
     {
-        internal readonly AppDbContext _context;
+        _context = context;
+    }
 
-        public BlogRepository(AppDbContext context)
+    #region GetBlogListAsync
+
+    public async Task<Result<List<BlogModel>>> GetBlogListAsync(int pageNo, int pageSize, CancellationToken cs)
+    {
+        Result<List<BlogModel>> result;
+        try
         {
-            _context = context;
-        }
-
-        #region GetBlogListAsync
-
-        public async Task<Result<List<BlogModel>>> GetBlogListAsync(int pageNo, int pageSize, CancellationToken cs)
-        {
-            Result<List<BlogModel>> result;
-            try
-            {
-                var query = _context.TblBlogs.Where(x => x.IsActive == true).Skip(pageNo - 1).Take(pageSize);
-                var lst = await query.Select(x => new BlogModel()
-                {
-                    BlogId = x.BlogId,
-                    BlogTitle = x.BlogTitle,
-                    BlogAuthor = x.BlogAuthor,
-                    BlogContent = x.BlogContent,
-                    IsActive = true
-
-                }).ToListAsync(cs);
-
-                result = Result<List<BlogModel>>.Success(lst);
-            }
-            
-            catch (Exception ex)
-            {
-                result = Result<List<BlogModel>>.Fail(ex);
-            }
-            return result;
-        }
-
-        #endregion
-
-        #region GetBlogListAsyncV1
-
-        public async Task<Result<List<BlogModel>>> GetBlogListAsyncV1(int pageNo, int pageSize, CancellationToken cs)
-        {
-            Result<List<BlogModel>> result;
             var query = _context.TblBlogs.Where(x => x.IsActive == true).Skip(pageNo - 1).Take(pageSize);
             var lst = await query.Select(x => new BlogModel()
             {
@@ -61,11 +28,38 @@ namespace MTKDotNetCoreAdvancedC_.RepositoryDesignPattern.Persistance.Repositori
             }).ToListAsync(cs);
 
             result = Result<List<BlogModel>>.Success(lst);
-
-            return result;
         }
-
-        #endregion
-
+        
+        catch (Exception ex)
+        {
+            result = Result<List<BlogModel>>.Fail(ex);
+        }
+        return result;
     }
+
+    #endregion
+
+    #region GetBlogListAsyncV1
+
+    public async Task<Result<List<BlogModel>>> GetBlogListAsyncV1(int pageNo, int pageSize, CancellationToken cs)
+    {
+        Result<List<BlogModel>> result;
+        var query = _context.TblBlogs.Where(x => x.IsActive == true).Skip(pageNo - 1).Take(pageSize);
+        var lst = await query.Select(x => new BlogModel()
+        {
+            BlogId = x.BlogId,
+            BlogTitle = x.BlogTitle,
+            BlogAuthor = x.BlogAuthor,
+            BlogContent = x.BlogContent,
+            IsActive = true
+
+        }).ToListAsync(cs);
+
+        result = Result<List<BlogModel>>.Success(lst);
+
+        return result;
+    }
+
+    #endregion
+
 }
