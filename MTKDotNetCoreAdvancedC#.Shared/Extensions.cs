b5b7f5a60caf32d399c88693fffe26560abe0dcc
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-03 12:53:44 +0630

    Feat : Add Extension

---
+++ MTKDotNetCoreAdvancedC#.Shared/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTKDotNetCoreAdvancedC_.Shared
+{
+    public static class Extensions
+    {
+        public static IQueryable<TSource> Paginate<TSource>(this IQueryable<TSource> sources, int pageNo, int pageSize)
+        {
+            return sources.Skip((pageNo-1)*pageSize).Take(pageSize);
+        } 
+    }
+}
