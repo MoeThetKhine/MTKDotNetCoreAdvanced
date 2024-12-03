310058d3631ee495fe252b10d6c9840c362f8639
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-03 17:17:48 +0630

    -

--- MTKDotNetCoreAdvancedC#.RepositoryDesignPattern/Persistance/Repositories/IBlogRepository.cs
+++ MTKDotNetCoreAdvancedC#.RepositoryDesignPattern/Persistance/Repositories/IBlogRepository.cs
@@ -5,7 +5,7 @@ public interface IBlogRepository
     Task<Result<List<BlogModel>>> GetBlogListAsync(int pageNo, int pageSize, CancellationToken cs);
     Task<Result<List<BlogModel>>> GetBlogListAsyncV1(int pageNo, int pageSize, CancellationToken cs);
     Task<Result<BlogResponseModel>> CreateBlogAsync(BlogResponseModel responseModel,CancellationToken cs);
-    Task<Result<BlogModel>> UpdateBlogAsync(int blogId, BlogModel blog, CancellationToken cs);
+    Task<Result<BlogResponseModel>> UpdateBlogAsync(int blogId, BlogResponseModel blog, CancellationToken cs);
 
 
 
