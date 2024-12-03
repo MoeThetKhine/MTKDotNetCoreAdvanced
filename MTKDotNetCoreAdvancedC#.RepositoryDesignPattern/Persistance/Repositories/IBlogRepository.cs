eb99b027463e93917537ad7e97c4aeb8bdd8ab02
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-03 10:07:41 +0630

    Feat : Add GetBlogListAsync in IBlogRepository

--- MTKDotNetCoreAdvancedC#.RepositoryDesignPattern/Persistance/Repositories/IBlogRepository.cs
+++ MTKDotNetCoreAdvancedC#.RepositoryDesignPattern/Persistance/Repositories/IBlogRepository.cs
@@ -1,6 +1,10 @@
-﻿namespace MTKDotNetCoreAdvancedC_.RepositoryDesignPattern.Persistance.Repositories
+﻿using MTKDotNetCoreAdvancedC_.RepositoryDesignPattern.Models;
+using MTKDotNetCoreAdvancedC_.Utils;
+
+namespace MTKDotNetCoreAdvancedC_.RepositoryDesignPattern.Persistance.Repositories
 {
     public interface IBlogRepository
     {
+        Task<Result<List<BlogModel>>> GetBlogListAsync(int pageNo, int pageSize, CancellationToken cs);
     }
 }
