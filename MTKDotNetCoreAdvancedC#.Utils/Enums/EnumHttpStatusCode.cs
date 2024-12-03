226cc96eb11ac5f850edc16ff76706883ff5d869
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-03 09:26:04 +0630

    Feat : Add EnumHttpStatusCode

---
+++ MTKDotNetCoreAdvancedC#.Utils/Enums/EnumHttpStatusCode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTKDotNetCoreAdvancedC_.Utils.Enum
+{
+    public enum EnumHttpStatusCode
+    {
+        None,
+        Success = 200,
+        BadRequest = 400,
+        Conflict = 409,
+        NotFound = 404,
+        InternalServerError = 500
+    }
+}
