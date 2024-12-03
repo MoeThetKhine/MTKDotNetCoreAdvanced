bda699205e2b48dee09c3b08c4f31b381458aca4
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-03 16:56:47 +0630

    Feat : Add UpdateBlogAsync

--- MTKDotNetCoreAdvancedC#.RepositoryDesignPattern/Persistance/Repositories/IBlogRepository.cs
+++ MTKDotNetCoreAdvancedC#.RepositoryDesignPattern/Persistance/Repositories/IBlogRepository.cs
@@ -5,5 +5,8 @@ public interface IBlogRepository
     Task<Result<List<BlogModel>>> GetBlogListAsync(int pageNo, int pageSize, CancellationToken cs);
     Task<Result<List<BlogModel>>> GetBlogListAsyncV1(int pageNo, int pageSize, CancellationToken cs);
     Task<Result<BlogResponseModel>> CreateBlogAsync(BlogResponseModel responseModel,CancellationToken cs);
+    Task<Result<BlogModel>> UpdateBlogAsync(int blogId, BlogModel blog, CancellationToken cs);
+
+
 
 }
