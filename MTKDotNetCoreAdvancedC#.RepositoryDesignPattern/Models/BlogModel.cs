3603d6bbeca1d98029206aa84d415ea07c9382fd
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-03 09:16:11 +0630

    Feat : Add BlogModel

---
+++ MTKDotNetCoreAdvancedC#.RepositoryDesignPattern/Models/BlogModel.cs
@@ -0,0 +1,15 @@
+﻿namespace MTKDotNetCoreAdvancedC_.RepositoryDesignPattern.Models
+{
+    public class BlogModel
+    {
+        public long BlogId { get; set; }
+
+        public string BlogTitle { get; set; } = null!;
+
+        public string BlogAuthor { get; set; } = null!;
+
+        public string BlogContent { get; set; } = null!;
+
+        public bool IsActive { get; set; }
+    }
+}
