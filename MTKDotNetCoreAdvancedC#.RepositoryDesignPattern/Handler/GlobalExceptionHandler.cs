41be48e1f6c2b1419f13437857e5e398c7d3dc50
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-03 12:12:55 +0630

    Feat : Global ExceptionHandler

---
+++ MTKDotNetCoreAdvancedC#.RepositoryDesignPattern/Handler/GlobalExceptionHandler.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using MTKDotNetCoreAdvancedC_.Utils;
+using MTKDotNetCoreAdvancedC_.Utils.Enum;
+
+namespace MTKDotNetCoreAdvancedC_.RepositoryDesignPattern.Handler
+{
+    public class GlobalExceptionHandler : IExceptionHandler
+    {
+
+        #region TryHandleAsync
+
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+           var result =  Result<object>.Fail(exception);
+            httpContext.Response.StatusCode = (int)EnumHttpStatusCode.Success;
+
+            await httpContext.Response.WriteAsJsonAsync(result,cancellationToken);
+
+            return true;
+        }
+
+        #endregion
+    }
+}
