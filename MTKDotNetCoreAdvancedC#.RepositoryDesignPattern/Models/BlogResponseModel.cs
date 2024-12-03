f6f02c06c58d1f3a5a0c20ce7e18a9e17de45eb4
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-03 15:09:46 +0630

    Feat : Add BlogResponseModel

---
+++ MTKDotNetCoreAdvancedC#.RepositoryDesignPattern/Models/BlogResponseModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MTKDotNetCoreAdvancedC_.RepositoryDesignPattern.Models
+{
+    public class BlogResponseModel
+    {
+        public string BlogTitle { get; set; } = null!;
+
+        public string BlogAuthor { get; set; } = null!;
+
+        public string BlogContent { get; set; } = null!;
+
+        public bool IsActive { get; set; }
+    }
+}
