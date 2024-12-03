0910b36110e21573e7d061cc411f0e74839d61f7
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-03 15:10:49 +0630

    Feat : Add CreateBlogAsync in IBlogRepository

--- MTKDotNetCoreAdvancedC#.RepositoryDesignPattern/Persistance/Repositories/IBlogRepository.cs
+++ MTKDotNetCoreAdvancedC#.RepositoryDesignPattern/Persistance/Repositories/IBlogRepository.cs
@@ -4,5 +4,6 @@ public interface IBlogRepository
 {
     Task<Result<List<BlogModel>>> GetBlogListAsync(int pageNo, int pageSize, CancellationToken cs);
     Task<Result<List<BlogModel>>> GetBlogListAsyncV1(int pageNo, int pageSize, CancellationToken cs);
+    Task<Result<BlogResponseModel>> CreateBlogAsync(BlogResponseModel responseModel,CancellationToken cs);
 
 }
