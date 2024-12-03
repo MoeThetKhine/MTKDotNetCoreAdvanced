e51b4bc4653b30d6584ff5749d918b2faaa99785
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-03 12:26:46 +0630

    -

--- MTKDotNetCoreAdvancedC#.Database/Models/TblBlog.cs
+++ MTKDotNetCoreAdvancedC#.Database/Models/TblBlog.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace MTKDotNetCoreAdvancedC_.Database.Models;
+﻿namespace MTKDotNetCoreAdvancedC_.Database.Models;
 
 public partial class TblBlog
 {
