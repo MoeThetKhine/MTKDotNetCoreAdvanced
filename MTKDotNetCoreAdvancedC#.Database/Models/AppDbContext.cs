ca1cb70abc5f02f05f5bfec74db8414c662bb608
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-03 09:14:05 +0630

    Feat : Add AppDbContext using Scaffold

---
+++ MTKDotNetCoreAdvancedC#.Database/Models/AppDbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MTKDotNetCoreAdvancedC_.Database.Models;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TblBlog> TblBlogs { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=.;Database=AdvancedC#;User Id=sa;Password=sasa@123;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TblBlog>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable("Tbl_blog");
+
+            entity.Property(e => e.BlogAuthor).HasMaxLength(50);
+            entity.Property(e => e.BlogContent).HasMaxLength(50);
+            entity.Property(e => e.BlogTitle).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
