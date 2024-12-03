71a1378e229d52953825da8ec2a7a0290f2139d0
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-03 12:35:39 +0630

    Format Code

--- MTKDotNetCoreAdvancedC#.RepositoryDesignPattern/Extensions/DependencyInjection.cs
+++ MTKDotNetCoreAdvancedC#.RepositoryDesignPattern/Extensions/DependencyInjection.cs
@@ -1,29 +1,28 @@
-﻿namespace MTKDotNetCoreAdvancedC_.RepositoryDesignPattern.Extensions
+﻿namespace MTKDotNetCoreAdvancedC_.RepositoryDesignPattern.Extensions;
+
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+
+    #region AddDependencies
+
+    public static IServiceCollection AddDependencies(this IServiceCollection services, WebApplicationBuilder builder)
     {
-
-        #region AddDependencies
-
-        public static IServiceCollection AddDependencies(this IServiceCollection services, WebApplicationBuilder builder)
+        services.AddDbContext<AppDbContext>(opt =>
         {
-            services.AddDbContext<AppDbContext>(opt =>
-            {
-                opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-            });
+            opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        });
 
-            services.AddScoped<IBlogRepository, BlogRepository>();
-            services.AddControllers().AddJsonOptions(opt =>
-            {
-                opt.JsonSerializerOptions.PropertyNamingPolicy = null;
-            });
+        services.AddScoped<IBlogRepository, BlogRepository>();
+        services.AddControllers().AddJsonOptions(opt =>
+        {
+            opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+        });
 
-            builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-            builder.Services.AddProblemDetails();
+        builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+        builder.Services.AddProblemDetails();
 
-            return services;
-        }
-
-        #endregion
+        return services;
     }
+
+    #endregion
 }
