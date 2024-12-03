c438511ff61b7bf17d71dfde6fb90b7606e32525
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-03 12:34:29 +0630

    Format Code

--- MTKDotNetCoreAdvancedC#.RepositoryDesignPattern/Controllers/BlogController.cs
+++ MTKDotNetCoreAdvancedC#.RepositoryDesignPattern/Controllers/BlogController.cs
@@ -1,38 +1,36 @@
-﻿
-namespace MTKDotNetCoreAdvancedC_.RepositoryDesignPattern.Controllers
+﻿namespace MTKDotNetCoreAdvancedC_.RepositoryDesignPattern.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BlogController : ControllerBase
+    internal readonly IBlogRepository _blogRepository;
+
+    public BlogController(IBlogRepository blogRepository)
     {
-        internal readonly IBlogRepository _blogRepository;
-
-        public BlogController(IBlogRepository blogRepository)
-        {
-            _blogRepository = blogRepository;
-        }
-
-        #region GetBlogListAsync
-
-        [HttpGet("list")]
-        public async Task<IActionResult> GetBlogListAsync(int pageNo, int pageSize,CancellationToken cs)
-        {
-            var lst = await _blogRepository.GetBlogListAsync(pageNo, pageSize, cs);
-            return Ok(lst);
-        }
-
-        #endregion
-
-        #region GetBlogListAsyncV1
-
-        [HttpGet("list-v1")]
-        public async Task<IActionResult> GetBlogListAsyncV1(int pageNo, int pageSize, CancellationToken cs)
-        {
-            var lst = await _blogRepository.GetBlogListAsyncV1(pageNo, pageSize, cs);
-            return Ok(lst);
-        }
-
-        #endregion
-
+        _blogRepository = blogRepository;
     }
+
+    #region GetBlogListAsync
+
+    [HttpGet("list")]
+    public async Task<IActionResult> GetBlogListAsync(int pageNo, int pageSize,CancellationToken cs)
+    {
+        var lst = await _blogRepository.GetBlogListAsync(pageNo, pageSize, cs);
+        return Ok(lst);
+    }
+
+    #endregion
+
+    #region GetBlogListAsyncV1
+
+    [HttpGet("list-v1")]
+    public async Task<IActionResult> GetBlogListAsyncV1(int pageNo, int pageSize, CancellationToken cs)
+    {
+        var lst = await _blogRepository.GetBlogListAsyncV1(pageNo, pageSize, cs);
+        return Ok(lst);
+    }
+
+    #endregion
+
 }
