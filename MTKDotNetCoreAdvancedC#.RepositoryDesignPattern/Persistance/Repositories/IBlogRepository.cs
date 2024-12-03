444e8b30daa63c7c7d72f9c373cbdb0d48f23e11
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-03 19:58:01 +0630

    Feat : Add DeleteBlog in IBlogRepository

--- MTKDotNetCoreAdvancedC#.RepositoryDesignPattern/Persistance/Repositories/IBlogRepository.cs
+++ MTKDotNetCoreAdvancedC#.RepositoryDesignPattern/Persistance/Repositories/IBlogRepository.cs
@@ -6,4 +6,5 @@ public interface IBlogRepository
     Task<Result<List<BlogModel>>> GetBlogListAsyncV1(int pageNo, int pageSize, CancellationToken cs);
     Task<Result<BlogResponseModel>> CreateBlogAsync(BlogResponseModel responseModel,CancellationToken cs);
     Task<Result<BlogResponseModel>> UpdateBlogAsync(int blogId, BlogResponseModel blog, CancellationToken cs);
+    Task<Result<BlogModel>> DeleteBlogAsync(int blogId, CancellationToken cs);
 }
