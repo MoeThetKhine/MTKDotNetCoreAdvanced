82aac4a2cf0a5f38804d3d16efcd22413a420403
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-03 12:37:25 +0630

    Format Code

--- MTKDotNetCoreAdvancedC#.RepositoryDesignPattern/Models/BlogModel.cs
+++ MTKDotNetCoreAdvancedC#.RepositoryDesignPattern/Models/BlogModel.cs
@@ -1,15 +1,14 @@
-﻿namespace MTKDotNetCoreAdvancedC_.RepositoryDesignPattern.Models
+﻿namespace MTKDotNetCoreAdvancedC_.RepositoryDesignPattern.Models;
+
+public class BlogModel
 {
-    public class BlogModel
-    {
-        public long BlogId { get; set; }
+    public long BlogId { get; set; }
 
-        public string BlogTitle { get; set; } = null!;
+    public string BlogTitle { get; set; } = null!;
 
-        public string BlogAuthor { get; set; } = null!;
+    public string BlogAuthor { get; set; } = null!;
 
-        public string BlogContent { get; set; } = null!;
+    public string BlogContent { get; set; } = null!;
 
-        public bool IsActive { get; set; }
-    }
+    public bool IsActive { get; set; }
 }
