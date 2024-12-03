aee1ef34e641715f072608bed9b20e7f0d286933
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-03 10:58:35 +0630

    Feat : Add GetBlogListAsync in BlogRepository

--- MTKDotNetCoreAdvancedC#.RepositoryDesignPattern/Persistance/Repositories/BlogRepository.cs
+++ MTKDotNetCoreAdvancedC#.RepositoryDesignPattern/Persistance/Repositories/BlogRepository.cs
@@ -1,6 +1,43 @@
-﻿namespace MTKDotNetCoreAdvancedC_.RepositoryDesignPattern.Persistance.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using MTKDotNetCoreAdvancedC_.Database.Models;
+using MTKDotNetCoreAdvancedC_.RepositoryDesignPattern.Models;
+using MTKDotNetCoreAdvancedC_.Utils;
+
+namespace MTKDotNetCoreAdvancedC_.RepositoryDesignPattern.Persistance.Repositories
 {
-    public class BlogRepository
+    public class BlogRepository : IBlogRepository
     {
+        internal readonly AppDbContext _context;
+
+        public BlogRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Result<List<BlogModel>>> GetBlogListAsync(int pageNo, int pageSize, CancellationToken cs)
+        {
+            Result<List<BlogModel>> result;
+            try
+            {
+                var query = _context.TblBlogs.Where(x => x.IsActive == true).Skip(pageNo - 1).Take(pageSize);
+                var lst = await query.Select(x => new BlogModel()
+                {
+                    BlogId = x.BlogId,
+                    BlogTitle = x.BlogTitle,
+                    BlogAuthor = x.BlogAuthor,
+                    BlogContent = x.BlogContent,
+                    IsActive = true
+
+                }).ToListAsync(cs);
+
+                result = Result<List<BlogModel>>.Success(lst);
+            }
+            
+            catch (Exception ex)
+            {
+                result = Result<List<BlogModel>>.Fail(ex);
+            }
+            return result;
+        }
     }
 }
