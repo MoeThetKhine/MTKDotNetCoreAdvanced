8de5b801917fdefb66df9d7a981851d31deceaeb
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-07 17:27:15 +0630

    Feat : Add BlogRepository

---
+++ MTKDotNetCoreAdvancedC#.GenericRepositoryPattern/Persistance/Repository/BlogRepository.cs
@@ -0,0 +1,12 @@
+﻿using MTKDotNetCoreAdvancedC_.Database.Models;
+
+namespace MTKDotNetCoreAdvancedC_.GenericRepositoryPattern.Persistance.Repository
+{
+    public class BlogRepository : RepositoryBase<TblBlog>, IBlogRepository
+    {
+        public BlogRepository(AppDbContext context) : base(context)
+        { 
+
+        }
+    }
+}
