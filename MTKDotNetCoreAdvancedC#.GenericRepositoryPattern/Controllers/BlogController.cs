66e7818ac28dfd5b36b4d4e68fcaec78ae314d31
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-07 21:16:18 +0630

    Feat : Add GetBlogAsync

---
+++ MTKDotNetCoreAdvancedC#.GenericRepositoryPattern/Controllers/BlogController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MTKDotNetCoreAdvancedC_.Shared;
+
+namespace MTKDotNetCoreAdvancedC_.GenericRepositoryPattern.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        public readonly IBlogRepository _blogRepository;
+
+        public BlogController(IBlogRepository blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetBlogAsync(int pageNo, int pageSize,CancellationToken cs)
+        {
+            var query = _blogRepository.Query().Paginate(pageNo, pageSize);
+            var lst = await query.ToListAsync(cs);
+
+            return Ok(lst);
+        }
+    }
+}
