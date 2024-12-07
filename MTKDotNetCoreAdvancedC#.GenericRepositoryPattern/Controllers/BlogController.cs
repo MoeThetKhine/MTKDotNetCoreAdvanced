ea795ecfe1a3af9bf5838ebc5c514aaf38a494d3
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-07 21:17:30 +0630

    Format Code

--- MTKDotNetCoreAdvancedC#.GenericRepositoryPattern/Controllers/BlogController.cs
+++ MTKDotNetCoreAdvancedC#.GenericRepositoryPattern/Controllers/BlogController.cs
@@ -1,27 +1,26 @@
-﻿namespace MTKDotNetCoreAdvancedC_.GenericRepositoryPattern.Controllers
+﻿namespace MTKDotNetCoreAdvancedC_.GenericRepositoryPattern.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BlogController : ControllerBase
+    public readonly IBlogRepository _blogRepository;
+
+    public BlogController(IBlogRepository blogRepository)
     {
-        public readonly IBlogRepository _blogRepository;
-
-        public BlogController(IBlogRepository blogRepository)
-        {
-            _blogRepository = blogRepository;
-        }
-
-        #region GetBlogAsync
-
-        [HttpGet]
-        public async Task<IActionResult> GetBlogAsync(int pageNo, int pageSize,CancellationToken cs)
-        {
-            var query = _blogRepository.Query().Paginate(pageNo, pageSize);
-            var lst = await query.ToListAsync(cs);
-
-            return Ok(lst);
-        }
-
-        #endregion
+        _blogRepository = blogRepository;
     }
+
+    #region GetBlogAsync
+
+    [HttpGet]
+    public async Task<IActionResult> GetBlogAsync(int pageNo, int pageSize,CancellationToken cs)
+    {
+        var query = _blogRepository.Query().Paginate(pageNo, pageSize);
+        var lst = await query.ToListAsync(cs);
+
+        return Ok(lst);
+    }
+
+    #endregion
 }
