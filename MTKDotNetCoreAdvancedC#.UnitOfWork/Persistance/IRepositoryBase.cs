7c8abeafadef4ffcd3b27e43fbc36dd3747f49b5
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-09 00:47:26 +0630

    Format Code

--- MTKDotNetCoreAdvancedC#.UnitOfWork/Persistance/IRepositoryBase.cs
+++ MTKDotNetCoreAdvancedC#.UnitOfWork/Persistance/IRepositoryBase.cs
@@ -1,20 +1,19 @@
 ﻿using System.Linq.Expressions;
 
-namespace MTKDotNetCoreAdvancedC_.UnitOfWork.Persistance
+namespace MTKDotNetCoreAdvancedC_.UnitOfWork.Persistance;
+
+public interface IRepositoryBase<T> where T : class
 {
-    public interface IRepositoryBase<T> where T : class
-    {
-        IQueryable<T> Query(Expression<Func<T, bool>>? expression = null);
-        Task AddAsync(T entity, CancellationToken cs = default);
-        void Add(T entity);
-        Task AddRangeAsync(IEnumerable<T> entities, CancellationToken cs);
-        void AddRange(IEnumerable<T> entities);
-        void Update(T entity);
-        void UpdateRange(IEnumerable<T> entities);
-        void Delete(T entity);
-        void DeleteRange(IEnumerable<T> entities);
-        void Dispose();
-        Task SaveChangesAsync(CancellationToken cs = default);
-        void SaveChanges();
-    }
+    IQueryable<T> Query(Expression<Func<T, bool>>? expression = null);
+    Task AddAsync(T entity, CancellationToken cs = default);
+    void Add(T entity);
+    Task AddRangeAsync(IEnumerable<T> entities, CancellationToken cs);
+    void AddRange(IEnumerable<T> entities);
+    void Update(T entity);
+    void UpdateRange(IEnumerable<T> entities);
+    void Delete(T entity);
+    void DeleteRange(IEnumerable<T> entities);
+    void Dispose();
+    Task SaveChangesAsync(CancellationToken cs = default);
+    void SaveChanges();
 }
