47c2b098cd232b71eb9928341ee25e287c6a8287
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-09 01:27:08 +0630

    Feat : Add GetBlogsAsync

--- MTKDotNetCoreAdvancedC#.UnitOfWork/Controllers/BlogController.cs
+++ MTKDotNetCoreAdvancedC#.UnitOfWork/Controllers/BlogController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MTKDotNetCoreAdvancedC_.Shared;
+using MTKDotNetCoreAdvancedC_.UnitOfWork.Persistance;
 
 namespace MTKDotNetCoreAdvancedC_.UnitOfWork.Controllers
 {
@@ -6,5 +8,21 @@ namespace MTKDotNetCoreAdvancedC_.UnitOfWork.Controllers
     [ApiController]
     public class BlogController : ControllerBase
     {
+        internal readonly IUnitOfWork _unitOfWork;
+
+        public BlogController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetBlogsAsync(int pageNo, int pageSize, CancellationToken cs)
+        {
+            var query = _unitOfWork.BlogRepository.Query().Paginate(pageNo, pageSize);
+            var lst = await query.ToListAsync(cs);
+
+            return Ok(lst);
+        }
     }
 }
