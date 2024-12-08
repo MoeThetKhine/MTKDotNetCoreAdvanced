c570764bed8d0003852fd8f18883f00630bdadd0
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-09 01:02:45 +0630

    Feat : Add IUnitOfWork

--- MTKDotNetCoreAdvancedC#.UnitOfWork/Persistance/IUnitOfWork.cs
+++ MTKDotNetCoreAdvancedC#.UnitOfWork/Persistance/IUnitOfWork.cs
@@ -1,6 +1,9 @@
-﻿namespace MTKDotNetCoreAdvancedC_.UnitOfWork.Persistance
+﻿using MTKDotNetCoreAdvancedC_.UnitOfWork.Persistance.Repositories;
+
+namespace MTKDotNetCoreAdvancedC_.UnitOfWork.Persistance
 {
     public interface IUnitOfWork
     {
+        IBlogRepository BlogRepository { get; }
     }
 }
