502caf548899f1defbcacf19eee00f9992b10889
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-09 01:12:02 +0630

    -

--- MTKDotNetCoreAdvancedC#.UnitOfWork/Persistance/UnitOFWork.cs
+++ MTKDotNetCoreAdvancedC#.UnitOfWork/Persistance/UnitOFWork.cs
@@ -5,9 +5,14 @@ public class UnitOfWork : IUnitOfWork
     public readonly AppDbContext _context;
     public IBlogRepository BlogRepository { get; set; }
 
+    #region UnitOfWork
+
     public UnitOfWork(AppDbContext context)
     {
         _context = context;
         BlogRepository ??= new BlogRepository(context);
     }
+
+    #endregion
+
 }
