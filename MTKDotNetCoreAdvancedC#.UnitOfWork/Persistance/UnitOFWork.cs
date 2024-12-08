6d77f64399f857eab881bb2d6dc51a72ab6fea95
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-09 01:11:36 +0630

    Format Code

--- MTKDotNetCoreAdvancedC#.UnitOfWork/Persistance/UnitOFWork.cs
+++ MTKDotNetCoreAdvancedC#.UnitOfWork/Persistance/UnitOFWork.cs
@@ -1,14 +1,13 @@
-﻿namespace MTKDotNetCoreAdvancedC_.UnitOfWork.Persistance
-{
-    public class UnitOfWork : IUnitOfWork
-    {
-        public readonly AppDbContext _context;
-        public IBlogRepository BlogRepository { get; set; }
+﻿namespace MTKDotNetCoreAdvancedC_.UnitOfWork.Persistance;
 
-        public UnitOfWork(AppDbContext context)
-        {
-            _context = context;
-            BlogRepository ??= new BlogRepository(context);
-        }
+public class UnitOfWork : IUnitOfWork
+{
+    public readonly AppDbContext _context;
+    public IBlogRepository BlogRepository { get; set; }
+
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+        BlogRepository ??= new BlogRepository(context);
     }
 }
