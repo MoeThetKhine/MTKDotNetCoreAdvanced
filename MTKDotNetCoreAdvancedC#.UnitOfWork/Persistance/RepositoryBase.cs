b9af7db34215856a5850f3a828e7a7a882da5ac2
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-09 01:12:26 +0630

    Format Code

--- MTKDotNetCoreAdvancedC#.UnitOfWork/Persistance/RepositoryBase.cs
+++ MTKDotNetCoreAdvancedC#.UnitOfWork/Persistance/RepositoryBase.cs
@@ -1,74 +1,73 @@
-﻿namespace MTKDotNetCoreAdvancedC_.UnitOfWork.Persistance
+﻿namespace MTKDotNetCoreAdvancedC_.UnitOfWork.Persistance;
+
+public class RepositoryBase<T> : IRepositoryBase<T> where T : class
 {
-    public class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    internal readonly AppDbContext _context;
+    internal DbSet<T> _dbSet;
+
+    public RepositoryBase(AppDbContext context)
     {
-        internal readonly AppDbContext _context;
-        internal DbSet<T> _dbSet;
+        _context = context;
+        _dbSet = context.Set<T>();
+    }
 
-        public RepositoryBase(AppDbContext context)
-        {
-            _context = context;
-            _dbSet = context.Set<T>();
-        }
+    public void Add(T entity)
+    {
+        _dbSet.Add(entity);
+    }
 
-        public void Add(T entity)
-        {
-            _dbSet.Add(entity);
-        }
+    public async Task AddAsync(T entity, CancellationToken cs = default)
+    {
+        await _dbSet.AddAsync(entity, cs);
+    }
 
-        public async Task AddAsync(T entity, CancellationToken cs = default)
-        {
-            await _dbSet.AddAsync(entity, cs);
-        }
+    public void AddRange(IEnumerable<T> entities)
+    {
+        _dbSet.AddRange(entities);
+    }
 
-        public void AddRange(IEnumerable<T> entities)
-        {
-            _dbSet.AddRange(entities);
-        }
+    public async Task AddRangeAsync(IEnumerable<T> entities, CancellationToken cs)
+    {
+        await _dbSet.AddRangeAsync(entities, cs);
+    }
 
-        public async Task AddRangeAsync(IEnumerable<T> entities, CancellationToken cs)
-        {
-            await _dbSet.AddRangeAsync(entities, cs);
-        }
+    public void Delete(T entity)
+    {
+        _dbSet.Remove(entity);
+    }
 
-        public void Delete(T entity)
-        {
-            _dbSet.Remove(entity);
-        }
+    public void DeleteRange(IEnumerable<T> entities)
+    {
+        _dbSet.RemoveRange(entities);
+    }
 
-        public void DeleteRange(IEnumerable<T> entities)
-        {
-            _dbSet.RemoveRange(entities);
-        }
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
 
-        public void Dispose()
-        {
-            _context.Dispose();
-        }
+    public IQueryable<T> Query(Expression<Func<T, bool>>? expression = null)
+    {
+        return expression is null ? _dbSet.AsQueryable() : _dbSet.Where(expression);
+    }
 
-        public IQueryable<T> Query(Expression<Func<T, bool>>? expression = null)
-        {
-            return expression is null ? _dbSet.AsQueryable() : _dbSet.Where(expression);
-        }
+    public void SaveChanges()
+    {
+        _context.SaveChanges();
+    }
 
-        public void SaveChanges()
-        {
-            _context.SaveChanges();
-        }
+    public async Task SaveChangesAsync(CancellationToken cs = default)
+    {
+        await _context.SaveChangesAsync(cs);
+    }
 
-        public async Task SaveChangesAsync(CancellationToken cs = default)
-        {
-            await _context.SaveChangesAsync(cs);
-        }
+    public void Update(T entity)
+    {
+        _dbSet.Update(entity);
+    }
 
-        public void Update(T entity)
-        {
-            _dbSet.Update(entity);
-        }
-
-        public void UpdateRange(IEnumerable<T> entities)
-        {
-            _dbSet.UpdateRange(entities);
-        }
+    public void UpdateRange(IEnumerable<T> entities)
+    {
+        _dbSet.UpdateRange(entities);
     }
 }
