e1138bd2f193517d53175ce04d24bd881d28a2a4
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-09 01:28:27 +0630

    Format Code

--- MTKDotNetCoreAdvancedC#.UnitOfWork/Controllers/BlogController.cs
+++ MTKDotNetCoreAdvancedC#.UnitOfWork/Controllers/BlogController.cs
@@ -1,27 +1,26 @@
-﻿namespace MTKDotNetCoreAdvancedC_.UnitOfWork.Controllers
+﻿namespace MTKDotNetCoreAdvancedC_.UnitOfWork.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BlogController : ControllerBase
+    internal readonly IUnitOfWork _unitOfWork;
+
+    public BlogController(IUnitOfWork unitOfWork)
     {
-        internal readonly IUnitOfWork _unitOfWork;
-
-        public BlogController(IUnitOfWork unitOfWork)
-        {
-            _unitOfWork = unitOfWork;
-        }
-
-        #region GetBlogsAsync
-
-        [HttpGet]
-        public async Task<IActionResult> GetBlogsAsync(int pageNo, int pageSize, CancellationToken cs)
-        {
-            var query = _unitOfWork.BlogRepository.Query().Paginate(pageNo, pageSize);
-            var lst = await query.ToListAsync(cs);
-
-            return Ok(lst);
-        }
-
-        #endregion
+        _unitOfWork = unitOfWork;
     }
+
+    #region GetBlogsAsync
+
+    [HttpGet]
+    public async Task<IActionResult> GetBlogsAsync(int pageNo, int pageSize, CancellationToken cs)
+    {
+        var query = _unitOfWork.BlogRepository.Query().Paginate(pageNo, pageSize);
+        var lst = await query.ToListAsync(cs);
+
+        return Ok(lst);
+    }
+
+    #endregion
 }
