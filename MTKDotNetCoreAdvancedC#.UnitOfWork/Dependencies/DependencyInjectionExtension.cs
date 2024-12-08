7e8236ca275ca3632dbc90e1519f024266cbc1bf
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-08 23:50:52 +0630

    Feat :Add DependencyInjection

---
+++ MTKDotNetCoreAdvancedC#.UnitOfWork/Dependencies/DependencyInjectionExtension.cs
@@ -0,0 +1,26 @@
+﻿namespace MTKDotNetCoreAdvancedC_.UnitOfWork.Dependencies;
+
+public static class DependencyInjectionExtension
+{
+
+    #region DependencyInjectionExtension
+
+    public static IServiceCollection AddDependencies(this IServiceCollection services)
+    {
+        services.AddDbContext<AppDbContext>(opt =>
+        {
+            opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        });
+
+        services.AddControllers().AddJsonOptions(opt =>
+        {
+            opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+        });
+
+        services.AddScoped<IBlogRepository, BlogRepository>();
+
+        return services;
+    }
+
+    #endregion
+}
