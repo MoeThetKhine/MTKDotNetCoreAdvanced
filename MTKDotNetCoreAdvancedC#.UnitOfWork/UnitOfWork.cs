23e8eb59e86dd8a60bea0d90bbb769595daed8d9
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-09 19:48:17 +0630

    Feat : Add UnitOfWork

--- MTKDotNetCoreAdvancedC#.UnitOfWork/UnitOfWork.cs
+++ MTKDotNetCoreAdvancedC#.UnitOfWork/UnitOfWork.cs
@@ -1,6 +1,14 @@
 ﻿namespace MTKDotNetCoreAdvancedC_.UnitOfWork
 {
-    public class UnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
+        internal readonly AppDbContext _context;
+        public IBlogRepository BlogRepository { get; set; }
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+            BlogRepository ??= new BlogRepository(context);
+        }
     }
 }
