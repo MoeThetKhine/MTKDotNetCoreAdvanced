02594c22b39c02d6ef0cdcc39905a7ae58fb4a39
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-10 23:13:22 +0630

    -

---
+++ MTKDotNetCoreAdvancedC#.Serilog/Controllers/LogController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MTKDotNetCoreAdvancedC_.Serilog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogController : ControllerBase
+    {
+    }
+}
