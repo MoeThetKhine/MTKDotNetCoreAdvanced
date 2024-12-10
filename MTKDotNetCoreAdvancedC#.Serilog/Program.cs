45b96c8ce205f65bba7a1797fd4ff14b316721cc
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-10 23:35:27 +0630

    Feat : Update Program.cs

--- MTKDotNetCoreAdvancedC#.Serilog/Program.cs
+++ MTKDotNetCoreAdvancedC#.Serilog/Program.cs
@@ -1,6 +1,27 @@
+using Serilog;
+using Serilog.Formatting.Compact;
+using Serilog.Templates;
+
 var builder = WebApplication.CreateBuilder(args);
 
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.File(
+        new CompactJsonFormatter(),
+        "logs/RestApi.txt",
+        rollingInterval: RollingInterval.Minute
+    )
+    .WriteTo.Console(new ExpressionTemplate("[{@t:HH:mm:ss} {@l:u3} {SourceContext}] {@m}\n{@x}"))
+    //      .WriteTo
+    //.MSSqlServer(
+    //    connectionString: builder.Configuration.GetConnectionString("DbConnection"),
+    //    sinkOptions: new MSSqlServerSinkOptions { TableName = "Tbl_Logs", AutoCreateSqlTable = true })
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
 builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
