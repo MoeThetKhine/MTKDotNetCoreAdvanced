62e059a55dc7c28abbd597745602f6cca66437d7
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-23 17:22:44 +0630

    Format Code

--- MTKDotNetCoreAdvancedC#.RestClient/Program.cs
+++ MTKDotNetCoreAdvancedC#.RestClient/Program.cs
@@ -1,39 +1,37 @@
-﻿
-namespace MTKDotNetCoreAdvancedC_.RestClient
+﻿namespace MTKDotNetCoreAdvancedC_.RestClient;
+
+public class Program
 {
-    public class Program
+    public static async Task Main(string[] args)
     {
-        public static async Task Main(string[] args)
+        RestClientExample restClientExample = new();
+        var lst = await restClientExample.GetAsync<List<BlogModel>>(
+            "https://localhost:7281",
+            $"/api/BlogEndpoint?pageNo={1}&pageSize={10}"
+        );
+        foreach (var item in lst!)
         {
-            RestClientExample restClientExample = new();
-            var lst = await restClientExample.GetAsync<List<BlogModel>>(
-                "https://localhost:7281",
-                $"/api/BlogEndpoint?pageNo={1}&pageSize={10}"
-            );
-            foreach (var item in lst!)
-            {
-                Console.WriteLine($"BlogId: {item.BlogId}");
-                Console.WriteLine($"Blog Title: {item.BlogTitle}");
-                Console.WriteLine($"Blog Author: {item.BlogAuthor}");
-                Console.WriteLine($"Blog Content: {item.BlogContent}");
-                Console.WriteLine("---------------------------------");
-            }
+            Console.WriteLine($"BlogId: {item.BlogId}");
+            Console.WriteLine($"Blog Title: {item.BlogTitle}");
+            Console.WriteLine($"Blog Author: {item.BlogAuthor}");
+            Console.WriteLine($"Blog Content: {item.BlogContent}");
+            Console.WriteLine("---------------------------------");
         }
     }
-
-    #region RestClientExample
-
-    public class RestClientExample
-    {
-        public async Task<T> GetAsync<T>(string url, string endpoint)
-        {
-            RestSharp.RestClient client = new(url);
-            RestRequest request = new(endpoint);
-            var lst = await client.GetAsync<T>(request);
-
-            return lst!;
-        }
-    }
-
-    #endregion
 }
+
+#region RestClientExample
+
+public class RestClientExample
+{
+    public async Task<T> GetAsync<T>(string url, string endpoint)
+    {
+        RestSharp.RestClient client = new(url);
+        RestRequest request = new(endpoint);
+        var lst = await client.GetAsync<T>(request);
+
+        return lst!;
+    }
+}
+
+#endregion
