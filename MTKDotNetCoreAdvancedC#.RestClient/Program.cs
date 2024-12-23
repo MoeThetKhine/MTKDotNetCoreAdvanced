f7e6afd8008fd3849e86418a09879bb1665174dd
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-23 15:49:49 +0630

    Feat : Add in Program.cs

--- MTKDotNetCoreAdvancedC#.RestClient/Program.cs
+++ MTKDotNetCoreAdvancedC#.RestClient/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RestSharp;
+
+namespace MTKDotNetCoreAdvancedC_.RestClient
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            RestClientExample restClientExample = new();
+            var lst = await restClientExample.GetAsync<List<BlogModel>>(
+                "https://localhost:7281",
+                $"/api/BlogEndpoint?pageNo={1}&pageSize={10}"
+            );
+            foreach (var item in lst!)
+            {
+                Console.WriteLine($"BlogId: {item.BlogId}");
+                Console.WriteLine($"Blog Title: {item.BlogTitle}");
+                Console.WriteLine($"Blog Author: {item.BlogAuthor}");
+                Console.WriteLine($"Blog Content: {item.BlogContent}");
+                Console.WriteLine("---------------------------------");
+            }
+        }
+    }
+
+   
+}
