3e2c67bf50df1373a6f49fa7b37022ee848666f3
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-07 10:21:15 +0630

    Feat : Add Change Model

---
+++ MTKDotNetCoreAdvancedC#.UnitOfWork/Models/ChangeModel.cs
@@ -0,0 +1,15 @@
+﻿namespace MTKDotNetCoreAdvancedC_.UnitOfWork.Models
+{
+    public static class ChangeModel
+    {
+        public static TblBlog Change(this BlogRequestModel requestModel)
+        {
+            return new TblBlog
+            {
+                BlogTitle = requestModel.BlogTitle,
+                BlogAuthor = requestModel.BlogAuthor,
+                BlogContent = requestModel.BlogContent,
+            };
+        }
+    }
+}
