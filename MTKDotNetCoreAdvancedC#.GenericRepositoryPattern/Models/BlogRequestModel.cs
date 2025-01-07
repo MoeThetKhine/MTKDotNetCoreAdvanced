b753ff8ecebc5abc446ffdb38861cd45f52726f4
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-07 15:08:08 +0630

    Feat : Add Blog Request Model

---
+++ MTKDotNetCoreAdvancedC#.GenericRepositoryPattern/Models/BlogRequestModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MTKDotNetCoreAdvancedC_.GenericRepositoryPattern.Models
+{
+    public class BlogRequestModel
+    {
+        public string BlogTitle { get; set; } = null!;
+
+        public string BlogAuthor { get; set; } = null!;
+
+        public string BlogContent { get; set; } = null!;
+    }
+}
