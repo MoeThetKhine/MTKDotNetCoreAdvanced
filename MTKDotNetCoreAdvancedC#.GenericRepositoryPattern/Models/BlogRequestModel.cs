24b085a0670c78bca232bf5ae4792e5e1de36bce
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-07 15:48:07 +0630

    Format Code

--- MTKDotNetCoreAdvancedC#.GenericRepositoryPattern/Models/BlogRequestModel.cs
+++ MTKDotNetCoreAdvancedC#.GenericRepositoryPattern/Models/BlogRequestModel.cs
@@ -1,11 +1,10 @@
-﻿namespace MTKDotNetCoreAdvancedC_.GenericRepositoryPattern.Models
+﻿namespace MTKDotNetCoreAdvancedC_.GenericRepositoryPattern.Models;
+
+public class BlogRequestModel
 {
-    public class BlogRequestModel
-    {
-        public string BlogTitle { get; set; } = null!;
+    public string BlogTitle { get; set; } = null!;
 
-        public string BlogAuthor { get; set; } = null!;
+    public string BlogAuthor { get; set; } = null!;
 
-        public string BlogContent { get; set; } = null!;
-    }
+    public string BlogContent { get; set; } = null!;
 }
